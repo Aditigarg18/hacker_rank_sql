9fa2760b985d391a420b6fc14bbc0b4efe73d46d
ADITI GARG <109287345+Aditigarg18@users.noreply.github.com>
2025-05-16 22:05:29 +0530

    Create 23.Revising Aggregations - The Count Function.sql

---
+++ 23.Revising Aggregations - The Count Function.sql
@@ -0,0 +1,6 @@
+-- Query a count of the number of cities in CITY having a Population larger than 100000.
+
+-- Input Format
+
+-- The CITY table is described as follows:
+select count(countrycode) from city where population>100000;
